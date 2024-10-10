cea987d6f847d55a6dfb5875e7256ea3a1ef0e14
cly05 <cly05@students.cs.ubc.ca>
2024-10-10 15:16:36 -0700

    Initial methods and specifications for Creature class.

--- src/main/model/characters/Creature.java
+++ src/main/model/characters/Creature.java
@@ -7,11 +7,46 @@ import model.items.Batwings;
 // This class represents the user's initial character.
 // This class will contain all the methods needed to give the user functionality
 public class Creature extends GameObject {
+
+    protected Inventory inventory;
+    protected boolean attackCooldown;
     
     // Requires: coordinate (x,y) must be within the bounds
     // Effects: Creates the Creature character with the name "????" as the creature never refers to himself with a title,
     //          at coordinate (x,y).
     public Creature(int x, int y) {
         super("????", x, y);
+        inventory = new Inventory();
     }
+
+    // Modifies: this
+    // Effects: Attacks the bat, if canAttack is true, and sets the specific bat isActive = false.
+    //          Has a cooldown of 4 seconds.
+    public boolean attack(Bats bats) {
+        // stub
+        return false;
+    }
+
+    // Effects: Checks if isInRange and attackCooldown = true
+    public boolean canAttack(Bats bats) {
+        // stub
+        return false;
+    }
+    
+
+    // Effects: Checks if gameObject is in range for the user to interact with it.
+    public boolean isInRange(GameObject gameObject) {
+        // stub
+        return false;
+    }
+
+    // Requires: must stay within bounds
+    // Modifies: this
+    // Effects: Moves the character in the direction of 'direction' a distance of 'distance'
+    public boolean move(String direction, int distance) {
+        // stub
+        return false;
+    }
+
+
 }
