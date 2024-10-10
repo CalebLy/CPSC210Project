944d621db9e43b3363151a282046dd7c489e02ec
cly05 <cly05@students.cs.ubc.ca>
2024-10-10 00:34:08 -0700

    Created a constructor for Creature and Batwings class.

--- src/main/model/characters/Creature.java
+++ src/main/model/characters/Creature.java
@@ -8,4 +8,10 @@ import model.items.Batwings;
 // This class will contain all the methods needed to give the user functionality
 public class Creature extends GameObject {
     
+    // Requires: coordinate (x,y) must be within the bounds
+    // Effects: Creates the Creature character with the name "????" as the creature never refers to himself with a title,
+    //          at coordinate (x,y).
+    public Creature(int x, int y) {
+        super("????", x, y);
+    }
 }
--- src/main/model/items/Batwings.java
+++ src/main/model/items/Batwings.java
@@ -5,7 +5,26 @@ public class Batwings extends GameObject{
     
     private String description;
     private int amount;
+    
+    
+    // Effects: Constructs an item with a static name of "Batwings". Coordinates (0,0) represent 
+    //          a meaningless position in the world as they will only be displayed in the inventory
+    public Batwings() {
+        super("Batwings",0,0);
+    }
 
+    // Modifies: this
+    // Effects: If there exists a Batwing in the user's inventory spawn rate of bats will increase, 
+    //          maximum occupancy of bats will increase, and it will return true.
+    //          If there doesn't exist a Batwing, it will return false.
+    public boolean useBatwing() {
+        // stub
+        return false;
+    }
+
+
+
+    // Getter methods
     public String getDescription() { 
         return description;
     }
