1ccfa160b83509b3c105d0af66d21f81bea75740
cly05 <cly05@students.cs.ubc.ca>
2024-10-11 06:56:33 -0700

    Implemented Main. End of phase 1.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,28 @@
 package ui;
 
+
+import java.util.Scanner;
+
+import model.characters.Bats;
+import model.characters.Creature;
+import model.rooms.Cave;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project! Test Commit");
+
+        boolean gameIsRunning = true;
+        Creature creature = new Creature(0, 0);
+        Cave cave = new Cave(30,30);
+        GameLoop gameLoop = new GameLoop();
+
+        creature.startAttackCooldown(creature.getAttackCooldownTime(), cave);
+        cave.spawnBats(cave.getMaxBats(), cave.getBatSpawnRate());
+
+        while (gameLoop.getGameIsRunning()) {
+            gameLoop.defaultLoopOptions(creature, cave);
+        }
+
+        
+
     }
 }
