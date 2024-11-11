057fcea62eb6ee18a3baa33bad4f8853e54134e3
cly05 <cly05@students.cs.ubc.ca>
2024-11-10 18:31:07 -0800

    Added Drawings for Bats and for the Creature and its different orientations.

--- src/main/ui/MyLabel.java
+++ src/main/ui/MyLabel.java
@@ -6,9 +6,9 @@ import javax.swing.ImageIcon;
 public class MyLabel extends JLabel {
     MyLabel() {
         this.setText("Frankenstein");
-        ImageIcon testImage = new ImageIcon("src\\main\\ui\\gui\\Icon\\Final Cropped Logo.png");
+        ImageIcon testImage = new ImageIcon("src\\main\\ui\\gui\\Creature\\CreatureFront.png");
         this.setIcon(testImage);
-        this.setBounds(600, 170, 400, 400);
+        this.setBounds(600, 0, 900, 700);
         this.setHorizontalTextPosition(JLabel.CENTER);
         this.setVerticalTextPosition(JLabel.TOP);
 
---
+++ src/main/ui/gui/Creature/CreatureGUI.java
@@ -0,0 +1,43 @@
+package ui.gui.Creature;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLabel;
+
+public class CreatureGUI extends JLabel implements MouseListener, ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    // Effects: Invoked when the mouse enters a component
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    // Effects: Invoked when the mouse exits a compoenent
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    
+}
